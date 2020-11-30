36bdf93ccc6630c003e7c8e4e3bfe52aedb681f7
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-12-01 01:25:36 +0900

    :bug: Fix BMD修正

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'annotate'
 gem 'config'
 gem 'slack-ruby-client'
 gem 'rails_autolink'
+gem 'jquery-rails'
+gem 'popper_js'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
-//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
-//= require jquery3
-//= require popper
