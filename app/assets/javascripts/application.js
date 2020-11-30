aa1eba065a2749526af4a1b74b80f7bbdc20eca9
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-11-30 13:25:44 +0900

    :sparkles: Add bootstarp-material-design 導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
+//= require jquery3
+//= require popper
