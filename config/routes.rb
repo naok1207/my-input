be7f18fc78c15f63f9a5b8a94d6025c02e4163cc
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-12-01 18:51:40 +0900

    :sparkles: Add 卒業研究用コマンド実行用API作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :qiitum, only: [] do
       post 'callback', to: 'qiitum#callback', on: :collection
     end
+    resources :graduation_research, only: [] do
+      post 'callback', to: 'graduation_research#callback', on: :collection
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
