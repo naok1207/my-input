82fcd9e57efe1133775be62e6f501d07efc22182
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-12-23 10:15:49 +0900

    Add slack_channelに関連したデータに修正

--- app/controllers/api/qiitum_controller.rb
+++ app/controllers/api/qiitum_controller.rb
@@ -5,10 +5,11 @@ class Api::QiitumController < ApplicationController
     when 'url_verification'
       render json: @body
     when 'event_callback'
-      if @body['event']['subtype'] == 'message_changed' 
+      if @body['event']['subtype'] == 'message_changed'
+        @channel = SlackChannel.channel(@body['event']['channel'])
         @body['event']['message']['attachments'].each do |attachment|
           next unless attachment['service_name'] == 'Qiita'
-          qiita = Qiitum.create(
+          qiita = @channel.qiitum.create(
             title:  attachment['title'],
             url:    attachment['from_url']
           )
